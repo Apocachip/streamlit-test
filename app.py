52340ea22a0ed9b12f8dcc0916335ad8776cd642
Choi Ji Hun <100457604+Apocachip@users.noreply.github.com>
2022-05-27 14:47:10 +0900

    add

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('data/GPU.csv', index_col=False)
+
+def main() :
+    menu = ['Home']
+
+
+
+if __name__ == '__main__' :
+    main()
